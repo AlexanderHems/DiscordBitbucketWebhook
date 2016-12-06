d97fa86c7bdc706b81ea40152f4592a5643e6fd7
Alexander Hems <ahems@dev5310.com>
2016-12-06 21:09:02 +0100

    add example json

--- index.js
+++ index.js
@@ -2,18 +2,30 @@ var express = require('express')
 var config = require('config');
 var request = require('request');
 var app = express();
-
+var json_example = {
+  "avatar_url" : 'https://www.brandeps.com/logo-download/B/Bitbucket-01.png',
+  "embeds": [{
+    "author" : {
+      "name" : 'Test Author',
+      "icon_url" : 'https://www.brandeps.com/logo-download/B/Bitbucket-01.png'
+    },
+    "title": 'Hello World!',
+    "description": 'Lorem ipsum dolon',
+    "type": 'rich',
+    "url" : 'https://www.google.com'
+  }]
+};
 
 
 app.get("/webhook/translator/bitbucket/to/default", function(req,res){
   var webhookUrl = config.get('discordWebhook');
   request.post(
     webhookUrl,
-    { json: { content: 'HELLO WORLD!' } },
+    { json: json_example },
     function (error, response, body) {
-        if (!error && response.statusCode == 200) {
-            console.log(body)
-        }
+
+            console.log(body,error)
+
     }
   );
   res.send("ok");
